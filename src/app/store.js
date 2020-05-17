213eea769d19e02e208cb20cd6f0382317fb3555
Evita <evitastenqvist@gmail.com>
2020-05-17 16:40:12 +0200

    Removed old counter reducer import

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import pokusReducer from '../features/pokus/pokusSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import pokusReducer from "../features/pokus/pokusSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     pokus: pokusReducer,
   },
 });
