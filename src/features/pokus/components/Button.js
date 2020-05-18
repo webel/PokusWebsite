22d2e8d71311c2d100f4771b47cedcbc5490d48d
Evita <evitastenqvist@gmail.com>
2020-05-18 09:11:08 +0200

    Added an onHover grow effect for Pokus button

--- src/features/pokus/components/Button.js
+++ src/features/pokus/components/Button.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import Radium from 'radium';
+import React from "react";
+import Radium from "radium";
+import { motion } from "framer-motion";
 
-const Button = ({onClick}) => (
-       <div style={styles} onClick={onClick} />
+const Button = ({ onClick }) => (
+  <motion.div
+    whileHover={{ scale: 1.05 }}
+    whileTap={{ scale: 0.95 }}
+    style={styles}
+    onClick={onClick}
+  />
 );
 
 const styles = {
-	position: 'relative',
-    left: '30%',
-    top: '30%',
+  position: "relative",
+  left: "30%",
+  top: "30%",
 
-    width: '90px',
-    height: '90px',
-    borderRadius: '50%',
-    background: '#333',
-    ':hover' : {
-        background: '#444'
-    }
+  width: "90px",
+  height: "90px",
+  borderRadius: "50%",
+  background: "#333",
+  ":hover": {
+    background: "#444",
+  },
 };
 
 export default Radium(Button);
